ada4126d690f10e2dfb0d2b4697a0e3922c747df
Monika Bergchauzen <tolamoon@gmail.com>
2020-01-22 12:40:08 +0100

    Chair - equals and hashCode stuff

---
+++ src/main/java/equals/Chair.java
@@ -0,0 +1,45 @@
+package equals;
+
+public class Chair {
+
+    private String model;
+    private String manufacturer;
+    private int productionYear;
+
+    public Chair(String model, String manufacturer, int productionYear) {
+        this.model = model;
+        this.manufacturer = manufacturer;
+        this.productionYear = productionYear;
+    }
+
+    @Override
+    public boolean equals(Object obj){
+//        if(obj==null)
+//            return false;
+        if(obj instanceof Chair){
+            Chair otherChair = (Chair) obj;
+            return model.equals(otherChair.model) &&
+                    manufacturer.equals(otherChair.manufacturer) &&
+                    productionYear==otherChair.productionYear;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode(){
+        return 17*model.hashCode() + 31*manufacturer.hashCode()+7*productionYear;
+    }
+
+    public static void main(String[] args){
+
+        Chair chair1 = new Chair("Adde", "IKEA", 2016);
+        Chair chair2 = new Chair("Janinge", "IKEA", 2016);
+        Chair chair3 = new Chair("Adde", "IKEA", 2015);
+        Chair chair4 = new Chair("Adde", "IKEA", 2016);
+
+        System.out.println("chair1.equals(chair2): " + chair1.equals(chair2));
+        System.out.println("chair1.equals(chair3): " + chair1.equals(chair3));
+        System.out.println("chair1.equals(chair4): " + chair1.equals(chair4));
+        System.out.println("chair1.equals(null): " + chair1.equals(null));
+    }
+}
