412253be542006c69f6e39e13a113fadfda485fc
Monika Bergchauzen <tolamoon@gmail.com>
2020-01-29 17:25:00 +0100

    Primality update

--- src/main/java/Primality.java
+++ src/main/java/Primality.java
@@ -1,21 +1,13 @@
-import java.util.Scanner;
-
 public class Primality {
-
-    public static void main(String[] args) {
-
-        int[] list = { 1,2,3,4,5,6,7,8,9,10,11 };
-        for(int i=0; i<list.length; i++){
-            System.out.println("Is given number prime ? "+i+" is "+ isPrime(i));
-        }
-    }
 
     private static boolean isPrime(int number){
         if(number<2)
             return false;
-        else if(number==2)
+        else if(number == 2)
             return true;
-        else if(number % 2 ==0)
+        else if(number % 2 == 0)
+            return false;
+        else if(number % 3 == 0)
             return false;
 
         int sqrt = (int)Math.sqrt(number);
@@ -26,4 +18,13 @@ public class Primality {
 
         return true;
     }
+
+    public static void main(String[] args) {
+
+        int[] list = { 0,1,2,3,4,5,6,7,8,9,10,11 };
+        for(int i=0; i<list.length; i++){
+            System.out.println(i + " is "+ isPrime(i));
+        }
+    }
+
 }
