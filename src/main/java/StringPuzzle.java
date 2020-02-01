aed0b33957a7d337058477d6cfbf1ba25ede2d6b
Monika Bergchauzen <tolamoon@gmail.com>
2020-02-01 18:53:40 +0100

    Sorting String in ascending order

---
+++ src/main/java/StringPuzzle.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class StringPuzzle {
+
+    private static String solution(String strIn) {
+        strIn = strIn.replace(".", "");
+        strIn = strIn.toLowerCase();
+        StringBuilder out = new StringBuilder();
+        List<String> list = new ArrayList<String>(Arrays.asList(strIn.split(" ")));
+
+        String current = list.get(0);
+
+        while(!list.isEmpty()){
+            current = list.get(0);
+            for(int i=1; i<list.size(); i++){
+                int elLen = list.get(i).length();
+                if(elLen < current.length()){
+                    current = list.get(i);
+                }
+            }
+            list.remove(current);
+            out.append(current).append(" ");
+        }
+        return out.toString();
+    }
+
+    public static void main(String args[]) {
+        String strIn = "Ala ma juz siedem kotow.";
+        System.out.print(solution(strIn));
+    }
+}
